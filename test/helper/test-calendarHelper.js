7ea6a00e588537958b2cb40e2f4a464a991d39b4
kurokisuisui <local.87.ume3@gmail.com>
2018-09-14 06:19:30 +0900

    test folder cd

--- test/helper/test-calendarHelper.js
+++
@@ -1,8 +0,0 @@
-var chai = require('chai');
-var should = chai.should();
-var expect = chai.expect;
-var calendarHelper = require('../../common/helper/calendarHelper');
-
-describe('calendarHelper test', function() {
-
-});
--- test/helper/test-errorHelper.js
+++
@@ -1,8 +0,0 @@
-var chai = require('chai');
-var should = chai.should();
-var expect = chai.expect;
-var errorHelper = require('../../common/helper/errorHelper');
-
-describe('errorHelper test', function() {
-
-});
