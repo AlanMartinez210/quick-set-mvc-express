0eef080a37e2e50a009febe5c822ad11d16b0c25
kurokisuisui <local.87.ume3@gmail.com>
2018-11-08 01:29:49 +0900

    正しくページャーが作動するように足りない仕様を追加

--- common/helper/pageHelper.js
+++ common/helper/pageHelper.js
@@ -7,9 +7,24 @@
  *
  */
 exports.makePageObject = (count, now_page)=>{
+
+  disp_page_Arr = []
+  const disp_page = 5;
+  const max_page = Number(count) > 0 ? Math.ceil(count/10) : 1;
+  const lim = Math.floor(disp_page/2) > 0 ? Math.floor(disp_page/2) : 1;
+  const res = Number(now_page) + lim ;
+  let dispMax = res > max_page ? max_page : res < disp_page ? disp_page : res;
+  dispMax += 1;
+  for(var i=disp_page;i>0;i--){
+    const c = (dispMax - i);
+    if(c <= 0) break;
+    disp_page_Arr.push(c);
+  }
+
   return {
     count: count,
     now_page: now_page,
-    max_page: Math.ceil(count/10),
+    max_page: max_page,
+    disp_page_list :disp_page_Arr
   };
 }
